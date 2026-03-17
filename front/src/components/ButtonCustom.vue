bd130a3f046feafe1650e23ed79c8ed6232896be
RomanNefedov <romanefedov2002@mail.ru>
2026-03-17 20:29:24 +0300

    Create ButtonCustom.vue

---
+++ front/src/components/ButtonCustom.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+    text: String,
+})
+</script>
+
+<template>
+
+    <button class="p-2 bg-white text-cyan-50 border-solid border-black border-2 rounded-lg">{{ text }}</button>
+</template>
+
+
+<style scoped>
+button {
+    transition: 0.2s;
+}
+
+button:hover {
+    transform: scale(1.1);
+}
+
+button:active {
+    transform: scale(0.9);
+}
+</style>
